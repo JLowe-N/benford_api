fcc37e0f3a4bfd6dee732ab89342b78b9e0164b4
Justin Lowen <Justin.G.Lowen@gmail.com>
2021-01-29 16:13:04 -0600

    Add additional API files

---
+++ Question1/README.md
@@ -0,0 +1,3 @@
+# Benford's Law API
+
+Use Tornado to visualize Benford's law using user-supplied data.
---
+++ Question1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="benford-api-JLowen", # Replace with your own username
+    version="0.0.1",
+    author="Justin Lowen",
+    author_email="Justin.G.Lowen@gmail.com",
+    description="Tornado based API to visualize Benford's Law",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
