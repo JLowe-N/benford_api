bbce6127e32932a305357acd6d9357461c3c500d
Justin Lowen <Justin.G.Lowen@gmail.com>
2021-01-30 14:24:49 -0600

    Extract first digit from column data succesful

--- Question1/benford_api/index.py
+++ Question1/benford_api/index.py
@@ -7,13 +7,24 @@ import pandas as pd
 
 class uploadRequestHandler(tornado.web.RequestHandler):
     async def post(self):
+        def extract_first_digit(number):
+            string = str(number)
+            first_char = string[0]
+            first_digit = int(first_char)
+            return first_digit
+
         input_file = self.request.files['input_file'][0]
         original_fname = input_file['filename']
         extension = os.path.splitext(original_fname)[1]
         body = input_file['body']
         print(extension)
         dataset = pd.read_csv(io.BytesIO(body), sep='\t')
-        print(dataset.head())
+        target_col = dataset['7_2009']
+        target_col = target_col.transform(extract_first_digit, axis=0)
+        # target_col = str(target_col)[0]
+        print(target_col.head())
+        print(target_col.describe())
+        
 
 
 class staticRequestHandler(tornado.web.RequestHandler):
