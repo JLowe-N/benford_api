b598fa185a973c4d742cdfd020f6759c54187674
Justin Lowen <Justin.G.Lowen@gmail.com>
2021-01-30 17:11:31 -0600

    Return PNG in response to POST request

--- Question1/benford_api/index.py
+++ Question1/benford_api/index.py
@@ -1,5 +1,4 @@
-import tornado.web
-import tornado.ioloop
+from tornado import web, iostream, gen, ioloop
 import os
 import io
 import pandas as pd
@@ -7,7 +6,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-class uploadRequestHandler(tornado.web.RequestHandler):
+class uploadRequestHandler(web.RequestHandler):
     async def post(self):
         def extract_first_digit(number):
             string = str(number)
@@ -28,10 +27,31 @@ class uploadRequestHandler(tornado.web.RequestHandler):
         print(target_col)
         ax = sns.barplot(
             x=target_col.index, y=target_col)
-        plt.show()
+
+        buf = io.BytesIO()
+        figure = ax.get_figure()
+        figure.savefig(buf, format="png")
+        buf.seek(0)
+
+        chunk_size = 1024 * 1024 * 1
+        self.set_header("Content-Type", "image/png")
+        while True:
+            chunk = buf.read(chunk_size)
+            if not chunk:
+                break
+            try:
+                print(chunk)
+                print(type(chunk))
+                self.write(chunk)
+                await self.flush()
+            except iostream.StreamClosedError:
+                break
+            finally:
+                del chunk
+        self.finish()
 
 
-class staticRequestHandler(tornado.web.RequestHandler):
+class staticRequestHandler(web.RequestHandler):
     def get(self):
         self.render(os.path.join(dirname, 'static/index.html'))
 
@@ -39,11 +59,11 @@ class staticRequestHandler(tornado.web.RequestHandler):
 if __name__ == '__main__':
     dirname = os.path.dirname(__file__)
     PORT = 8881
-    app = tornado.web.Application([
+    app = web.Application([
         (r"/", staticRequestHandler),
         (r"/upload", uploadRequestHandler)
     ])
 
     app.listen(PORT)
     print(f"Tornado listening on port {PORT}")
-    tornado.ioloop.IOLoop.current().start()
+    ioloop.IOLoop.current().start()
