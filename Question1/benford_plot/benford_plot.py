c4727a366725141a785a01a4e76d3ba8532e3131
Justin Lowen <Justin.G.Lowen@gmail.com>
2021-02-01 16:19:06 -0600

    Refactor to separate p-val calc

--- Question1/benford_plot/benford_plot.py
+++ Question1/benford_plot/benford_plot.py
@@ -26,7 +26,7 @@ def get_benford_column(file_body, target_col_header=None):
     # extract leading digit from target column, drop 0 values
     dataset = pd.read_csv(io.BytesIO(file_body), sep='\t')
     target_col = dataset['7_2009']
-    target_col = target_col.rename('user_data')
+    target_col = target_col.rename('actual_freq')
     target_col = target_col.transform(extract_first_digit, axis=0)
     target_col = target_col[(target_col.T != 0)]
 
@@ -35,32 +35,38 @@ def get_benford_column(file_body, target_col_header=None):
     observation_count = actual_freq.sum()
     expected_freq = pd.Series(
         [int(round(fraction * observation_count, 0)) for fraction in BENFORD_CURVE])
-
     print(expected_freq)
-    print(actual_freq)
-    print(chisquare(actual_freq, expected_freq))
 
     # convert counts to percentage
-    target_col = target_col.value_counts(normalize=True)
-    benford_df = target_col.to_frame()
-    benford_df['correlation'] = BENFORD_CURVE
-    benford_df = benford_df.reset_index()
+    benford_df = actual_freq.to_frame().reset_index()
+    benford_df['expected_freq'] = expected_freq
+    benford_df = benford_df.rename(columns={'index':'digit'})
     print(benford_df)
     return benford_df
 
 
+def get_p_value(actual_freq, observed_freq):
+    (_, p_value) = chisquare(actual_freq, observed_freq)
+    return p_value
+
+
 def get_benford_plot_src_str(benford_df):
+    # Calculate p_value for plot
+    print(benford_df)
+    p_val = get_p_value(benford_df['actual_freq'], benford_df['expected_freq'])
+    print(p_val)
     # Create combo chart
     fig, ax = plt.subplots(figsize=(10, 6))
     # bar plot from user data
-    sns.barplot(x=benford_df.index, y='user_data', data=benford_df)
+    sns.barplot(x='digit', y='actual_freq', data=benford_df)
     # line plot from Benford's Law
-    sns.lineplot(x=benford_df.index, y='correlation', data=benford_df,
+    sns.lineplot(x=benford_df.index, y='expected_freq', data=benford_df,
                  color="black", label="Benford's Law Distribution", linestyle="-")
     # annotate plot with labels
     plt.title("Benford's Law")
     plt.xlabel("Leading Digit")
     plt.ylabel("Frequency")
+    plt.gcf().text(0.02, 0.02, p_val, fontsize=14)
 
     # convert to base64 encoded PNG, for use in <img> src template
     stream = io.BytesIO()
