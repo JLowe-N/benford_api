9748c42f12cdbcf413fe51eb22cd8503ca047a85
Justin Lowen <Justin.G.Lowen@gmail.com>
2021-02-02 01:58:53 -0600

    Add template controls for result analysis

--- Question1/benford_api/app.py
+++ Question1/benford_api/app.py
@@ -1,19 +1,21 @@
 from tornado import web, iostream, gen, ioloop
 import os
-from benford_plot.benford_plot import get_benford_column, get_benford_plot_src_str
+from benford_plot.benford_plot import get_benford_column, get_benford_plot_src_str, get_p_value
 
 
 class benfordRequestHandler(web.RequestHandler):
     def get(self):
-        self.render('index.html', plot=None)
+        self.render('index.html', plot=None, pval=None)
 
     def post(self):
         input_file = self.request.files['input_file'][0]
         body = input_file['body']
         benford_col = get_benford_column(body)
+        p_value = round(get_p_value(
+            benford_col['actual_freq'], benford_col['expected_freq']), 3)
         plot_src = get_benford_plot_src_str(benford_col)
 
-        self.render('index.html', plot=plot_src)
+        self.render('index.html', plot=plot_src, pval=p_value)
 
 
 class iconRequestHandler(web.RequestHandler):
